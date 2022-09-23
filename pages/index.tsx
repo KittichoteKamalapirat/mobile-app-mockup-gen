a6125891fcab045c4c76feb0e5c93e3bb1348fd1
shane <shane@groundedpackaging.co>
2022-09-23 17:17:04 +0700

    can reset camera position

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,71 +1,52 @@
-import { useSpring, animated, config } from "@react-spring/three";
 import rgbaToRgb from "rgba-to-rgb";
 /* eslint-disable @next/next/no-img-element */
-import { ContactShadows, OrbitControls, useTexture } from "@react-three/drei";
-import { Canvas, useThree } from "@react-three/fiber";
-import { Suspense, useCallback, useRef, useState } from "react";
+import { ContactShadows, OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import {
+  forwardRef,
+  Suspense,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { RgbaStringColorPicker } from "react-colorful";
 import { useSelector } from "react-redux";
-import { Euler, Texture, WebGLCubeRenderTarget } from "three";
 import { proxy, useSnapshot } from "valtio";
 import useClickOutside from "../functions/src/hooks/useClickOutside";
-import { rgbaToHex } from "../functions/src/utils/rgbaToHex";
-import IPhone from "../src/components/3d/IPhone";
 import Iphone13Concept from "../src/components/3d/Iphone13Concept";
 import Button from "../src/components/Buttons/Button";
 import DropzoneField, { UploadedFile } from "../src/components/DropzoneField";
 import { RootState } from "../src/redux/store";
+import { Camera } from "three";
 
 interface Props {}
 
 const RGB_WHITE_BG = "rgb(255, 255, 255)";
 
-const PhoneScene = () => {
-  const upload: UploadedFile = useSelector((state: RootState) => state.upload);
-
-  // useControls({
-  //   screenshot: button(() => {
-  //     const link = document.createElement("a");
-  //     link.setAttribute("download", "canvas.png");
-  //     link.setAttribute(
-  //       "href",
-  //       gl.domElement
-  //         .toDataURL("image/png")
-  //         .replace("image/png", "image/octet-stream")
-  //     );
-  //     link.click();
-  //   }),
-  // });
-
-  return (
-    <>
-      <OrbitControls enableZoom={false} />
-      <ambientLight intensity={0.5} />
-      <spotLight intensity={0.3} position={[5, 20, 20]} />
-      <directionalLight position={[-2, 5, 2]} intensity={1} />
-      <animated.Suspense fallback={null}>
-        <IPhone upload={upload} />
-        {/* <Environment files="/threejs/royal_esplanade_1k.hdr" /> */}
-
-        <ContactShadows
-          position={[1, -2, 0]}
-          opacity={1}
-          scale={10}
-          blur={1}
-          far={0.8}
-        />
-      </animated.Suspense>
-    </>
-  );
-};
-
 export const CAMERA_DEFAULT_ROTATION = [0, 0, 0];
 export const OBJECT_DEFAULT_ROTATION = [-Math.PI / 2, 0, Math.PI];
 
-export const state = proxy({
+interface ProxyState {
+  canvaColor: string;
+  cameraRotationX: number;
+  cameraRotationY: number;
+  cameraRotationZ: number;
+  shadowIsOn: boolean;
+  bgIsTransparent: boolean;
+  camera: null | Camera;
+  objectRotationX: number;
+  objectRotationY: number;
+  objectRotationZ: number;
+}
+export const state: ProxyState = proxy({
+  canvaColor: "rgba(255,255,255,1)",
   cameraRotationX: 0,
   cameraRotationY: 0,
   cameraRotationZ: 25,
+  shadowIsOn: true,
+  bgIsTransparent: false,
+  camera: null,
 
   objectRotationX: -Math.PI / 2,
   objectRotationY: 0,
@@ -74,7 +55,6 @@ export const state = proxy({
 
 const ThreeDimension = ({}: Props) => {
   const [fileUploads, setFileUploads] = useState<UploadedFile[]>([]);
-  const [canvaColor, setCanvaColor] = useState("rgba(255,255,255,1)");
 
   const snap = useSnapshot(state);
 
@@ -86,13 +66,12 @@ const ThreeDimension = ({}: Props) => {
   );
 
   const [colorPickerIsOpen, setColorPickerIsOpen] = useState(false);
-  const [shadowIsOn, setShadowIsOn] = useState(true);
-  const [bgIsTransparent, setBgIsTransparent] = useState(false);
 
   const close = useCallback(() => setColorPickerIsOpen(false), []);
 
-  const toggleShadow = () => setShadowIsOn(!shadowIsOn);
-  const toggleTransparentBg = () => setBgIsTransparent(!bgIsTransparent);
+  const toggleShadow = () => (state.shadowIsOn = !state.shadowIsOn);
+  const toggleTransparentBg = () =>
+    (state.bgIsTransparent = !state.bgIsTransparent);
 
   const colorPickerRef = useRef<HTMLDivElement>(null);
   const canvaRef = useRef<HTMLCanvasElement>(null);
@@ -101,20 +80,16 @@ const ThreeDimension = ({}: Props) => {
 
   useClickOutside(colorPickerRef, close);
 
-  // create img from canvas
+  const handleCameraRotation = (camera: Camera) => {
+    camera.position.set(0, 0, 4);
+  };
 
+  // create img from canvas
   const handleDownload = () => {
     const canvas = canvaRef.current as HTMLCanvasElement;
 
-    // const ctx = canvas.getContext("2d");
-    // console.log("ctx", ctx);
-
     const url = canvas.toDataURL("image/png");
     const name = upload.name;
-
-    // create the phone image
-    // const ssImg = new Image();
-    // ssImg.src = upload.presignedUrl;
 
     const link = document.createElement("a");
     link.download = name;
@@ -122,134 +97,32 @@ const ThreeDimension = ({}: Props) => {
     link.click();
   };
 
-  const rgb = rgbaToRgb(RGB_WHITE_BG, canvaColor);
-
-  console.log("canvaRef", canvaRef);
-
   return (
     <div className="h-screen relative ">
-      {/* <Canvas className="h-screen">
-        <OrbitControls enableZoom={false} />
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[-2, 5, 2]} intensity={1} />
-        <Suspense fallback={null}>
-          <Cube />
-          <Environment files="/threejs/royal_esplanade_1k.hdr" />
-          <ContactShadows
-            // rotation-x={Math.PI / 2}
-            // position={[0, 0, 0]}
-            opacity={0.9}
-            width={10}
-            height={10}
-            blur={2}
-            far={0.1}
-          />
-        </Suspense>
-      </Canvas> */}
-
-      {/* <LinkButton
-        label="Back Home"
-        href="/"
-        type={ButtonTypes.TEXT}
-        leftIcon={<BiArrowBack size={20} />}
-        extraClass="z-10 absolute top-0 left-0 mt-4 ml-4"
-      /> */}
-
-      {/* <Canvas
-        style={{ height: "100vh", backgroundColor: canvaColor }}
-        gl={{ preserveDrawingBuffer: true }}
-        ref={canvaRef}
-        // camera={{ position: [2, 0, 12.25], fov: 15 }}
-      >
-        <OrbitControls enableZoom={false} />
-        <ambientLight intensity={0.5} />
-        <spotLight intensity={0.3} position={[5, 20, 20]} />
-        <directionalLight position={[-2, 5, 2]} intensity={1} />
-        <Suspense fallback={null}>
-          <IPhone upload={upload} />
-          <Environment files="/threejs/royal_esplanade_1k.hdr" />
-
-          <ContactShadows
-            position={[0, -0.8, 0]}
-            opacity={1}
-            scale={10}
-            blur={1}
-            far={0.8}
-          />
-        </Suspense>
-      </Canvas> */}
-
       <Canvas
+        ref={canvaRef}
         style={{
           height: "100vh",
           zIndex: 1,
         }}
-        ref={canvaRef}
         gl={{ preserveDrawingBuffer: true }}
-        camera={{
-          // position: [0, 0, 25], //x,y,z?
+        // camera={{
+        //   // position: [0, 0, 25], //x,y,z?
 
-          position: [
-            snap.cameraRotationX,
-            snap.cameraRotationY,
-            snap.cameraRotationZ,
-          ], //x,y,z?
-          fov: 15,
-        }} // x z y
+        //   position: [
+        //     snap.cameraRotationX,
+        //     snap.cameraRotationY,
+        //     snap.cameraRotationZ,
+        //   ], //x,y,z?
+        //   fov: 15,
+        // }} // x z y
       >
-        {!bgIsTransparent && <color attach="background" args={[rgb]} />}
-        <OrbitControls enableZoom={false} />
-        <ambientLight intensity={0.5} />
-        <spotLight intensity={0.3} position={[5, 0, -10]} />
-        <directionalLight
-          position={[-2, 5, 2]} //x,y,z
-          intensity={1}
-        />
-        <Suspense fallback={null}>
-          <Iphone13Concept upload={upload} />
-
-          {/* Floor */}
-          <group position={[0, 0, 0]}>
-            {/* <ContactShadows
-              // position={[0, -0.8, 0]} // x z y
-              // rotation-x={Math.PI / 2}
-              position={[0, -0.8, 0]} // x z y
-              opacity={1}
-              scale={10} // more => kinda more gradient and blue
-              blur={3}
-              far={0.8} // darkness: more => darker
-              // width={10}
-              // height={10}
-            />
-            <ContactShadows
-              position={[0, -1, 0]}
-              opacity={1}
-              scale={10}
-              blur={3}
-            />
-
-            <ContactShadows
-              position={[0, -2, 0]}
-              opacity={1}
-              scale={10}
-              blur={3}
-            /> */}
-
-            {shadowIsOn && (
-              <ContactShadows
-                position={[0, -1.66, 0]}
-                opacity={1}
-                scale={10}
-                blur={4}
-              />
-            )}
-          </group>
-        </Suspense>
+        <Scene upload={upload} />
       </Canvas>
 
       <div
         id="menu"
-        className="flex-col p-2 absolute top-1/4 bg-grey-0 shadow-xl z-10"
+        className="flex-col p-2 absolute top-10 bg-grey-0 shadow-xl z-10"
       >
         <DropzoneField
           ariaLabel="Image"
@@ -275,8 +148,8 @@ const ThreeDimension = ({}: Props) => {
         {colorPickerIsOpen && (
           <div ref={colorPickerRef} className="my-2">
             <RgbaStringColorPicker
-              color={canvaColor}
-              onChange={setCanvaColor}
+              color={snap.canvaColor}
+              onChange={(newColor) => (state.canvaColor = newColor)}
             />
           </div>
         )}
@@ -284,7 +157,7 @@ const ThreeDimension = ({}: Props) => {
         <div
           style={{
             backgroundClip: "content-box",
-            backgroundColor: canvaColor,
+            backgroundColor: snap.canvaColor,
             borderWidth: "1px",
           }}
           className="w-10 h-10 p-1 m-2  border-grey-300 border-solid rounded-sm bg-green box-content"
@@ -295,7 +168,10 @@ const ThreeDimension = ({}: Props) => {
           <Button
             label="reset camera rotation"
             onClick={() => {
-              state.cameraRotationIsReset = !state.cameraRotationIsReset;
+              const camera = snap.camera;
+              if (camera) {
+                handleCameraRotation(camera);
+              }
             }}
           />
         </div>
@@ -429,14 +305,50 @@ const ThreeDimension = ({}: Props) => {
   );
 };
 
-// const Background = () => {
-//   const { gl } = useThree();
-
-//   const texture = useTexture(transparentBg.src);
-//   const formatted = new WebGLCubeRenderTarget(
-//     texture.image.height
-//   ).fromEquirectangularTexture(gl, texture);
-//   return <primitive attach="background" object={formatted.texture} />;
-// };
-
 export default ThreeDimension;
+
+const deg2rad = (degrees: number) => degrees * (Math.PI / 180);
+
+interface SceneProps {
+  upload: UploadedFile;
+}
+const Scene = ({ upload }: SceneProps) => {
+  const snap = useSnapshot(state);
+  const rgb = rgbaToRgb(RGB_WHITE_BG, snap.canvaColor);
+
+  console.log("snap camera x", snap.cameraRotationX);
+  const { camera } = useThree();
+
+  useEffect(() => {
+    if (camera) state.camera = camera;
+  }, [camera]);
+
+  return (
+    <>
+      {!snap.bgIsTransparent && <color attach="background" args={[rgb]} />}
+      <OrbitControls enableZoom={true} enableRotate={true} />
+      <ambientLight intensity={0.5} />
+
+      {camera && <cameraHelper args={[camera]} />}
+      <spotLight intensity={0.3} position={[5, 0, -10]} />
+      <directionalLight
+        position={[-2, 5, 2]} //x,y,z
+        intensity={1}
+      />
+      <Suspense fallback={null}>
+        <Iphone13Concept upload={upload} />
+
+        <group position={[0, 0, 0]}>
+          {snap.shadowIsOn && (
+            <ContactShadows
+              position={[0, -1.66, 0]}
+              opacity={1}
+              scale={10}
+              blur={4}
+            />
+          )}
+        </group>
+      </Suspense>
+    </>
+  );
+};
